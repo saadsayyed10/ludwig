bc235b5a737b64b539d623300aed77fa6efb515b
Saad Sayyed <saadsyed950@gmail.com>
2026-05-12 09:08:06 +0530

    Prisma ORM instance

---
+++ backend/src/api/services/user.service.ts
@@ -0,0 +1,8 @@
+// backend/src/api/services/user.service.ts
+
+export const syncClerkUserService = async (
+  clerkId: string,
+  fullName: string,
+  email: string,
+  profilePicture: string,
+) => {};
